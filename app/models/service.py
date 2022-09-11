0826ec81731c516a9ce68094e74258b0a55b9631
Emanuel Aguirre Molina <emagui11572@gmail.com>
2022-09-10 22:52:09 -0500

    continuación de service.py

--- app/models/service.py
+++ app/models/service.py
@@ -11,6 +11,9 @@ class Service:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
         self.user_id = data['user_id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.nickname = data['nickname']
 
 
 
@@ -39,8 +42,8 @@ class Service:
         return es_valido
 
     @classmethod
-    def get_by_id(cls, formulario):#metodo de clase que obtiene todos los servicios de una determinada persona, enviandole en el formulario el id de esa persona 
-        query = "select * from services where user_id=%(id)s;"
+    def get_by_id(cls, formulario):#metodo de clase que obtiene todos los servicios de una determinada persona, su nombre, apellido y apodo, enviandole en el formulario el id de esa persona 
+        query = "select services.*, users.first_name, users.last_name, users.nickname from services left join users on users.id = services.user_id where user_id=%(id)s;"
         result = connectToMySQL('swappers').query_db(query, formulario) #Select recibe lista
         services = []
         for service in result:
@@ -48,24 +51,39 @@ class Service:
         return services #regresa una lista con cada uno de los servicios hechos instancias que posee dicho usuario 
         
 
-"""    @classmethod
-    def delete(cls, formulario): #delete
-        query= "Delete from appointments where id=%(id)s;"
+    @classmethod
+    def delete_service(cls, formulario): #metodo de clase que sirve para borrar un servicio, al cual debe enviarsele el id del servicio
+        query= "Delete from services where id=%(id)s;"
         result = connectToMySQL('swappers').query_db(query, formulario)
         return result
 
     @classmethod 
-    def update(cls, formulario): #
-
-        query= "update appointments set task=%(task)s, date=%(date)s, status=%(status)s where id=%(id)s"
+    def update_service(cls, formulario): #metodo de clase que sirve para actualizar un servicio, al cual debe enviarsele el id del servicio
+        query= "update services set name=%(name)s, type_service=%(type_service)s, description=%(description)s where id=%(id)s"
         result = connectToMySQL('swappers').query_db(query, formulario)
         return result
 
     @classmethod
-    def get_info(cls, formulario):
-        query = "select * from appointments where id=%(id)s;"
+    def filter_services_by_city(cls, formulario):#metodo de clase que obtiene todos los servicios de una determinada ciudad,  nombre, apellido y apodo de la persona poseedora del servicio, enviandole en el formulario la city que se necesita 
+        query = "select services.*, users.first_name, users.last_name, users.nickname from services left join users on users.id = services.user_id where city=%(city)s;"
         result = connectToMySQL('swappers').query_db(query, formulario) #Select recibe lista
-        return cls(result[0])"""
+        services = []
+        for service in result:
+            services.append(cls(service))
+        return services #regresa una lista con cada uno de los servicios hechos instancias que posee dicho usuario 
 
-#query para filtrar servicios por ciudad
-#query para filtrar servicios por categoria 
+    @classmethod
+    def filter_services_by_type_services(cls, formulario):#metodo de clase que obtiene todos los servicios de una determinada categoria,  nombre, apellido y apodo de la persona poseedora del servicio, enviandole en el formulario la categoría que se necesita 
+        query = "select services.*, users.first_name, users.last_name, users.nickname from services left join users on users.id = services.user_id where type_service=%(type_service)s;"
+        result = connectToMySQL('swappers').query_db(query, formulario) #Select recibe lista
+        services = []
+        for service in result:
+            services.append(cls(service))
+        return services #regresa una lista con cada uno de los servicios hechos instancias que posee dicho usuario 
+
+    @classmethod
+    def pre_match(cls, formulario):#metodo de clase para hacer match con el servicio de una persona, necesita enviarse el servicio que se quiere y el id del usuario que lo quiere
+        query = "INSERT INTO users_want_services (service_id, user_id) VALUES ('%(service_id)s', '%(user_id)s');"
+        result = connectToMySQL('swappers').query_db(query, formulario) 
+        return result 
+
