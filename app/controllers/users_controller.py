6a57a910bd1e33d52fc5398857ff12e1d957a987
Jonathan <yotomc17@gmail.com>
2022-09-13 20:29:08 -0500

    se crea base de login y register, se modifica css header

--- app/controllers/users_controller.py
+++ app/controllers/users_controller.py
@@ -7,3 +7,11 @@ bcrypt = Bcrypt(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
