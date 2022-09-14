8ae1b9c50922553cd78ee769392933138fb009a8
Jonathan <yotomc17@gmail.com>
2022-09-14 00:25:58 -0500

    se completan pantallas de login, registro y 404, se modifica estilo final del footer

--- app/controllers/users_controller.py
+++ app/controllers/users_controller.py
@@ -4,6 +4,10 @@ from flask import render_template, redirect, session, request, flash, jsonify
 from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404 #status200
+
 @app.route('/')
 def index():
     return render_template('index.html')
