ee84d1cc5ff507085aa4e632dd4a948a7976e1d1
EmanuelAM01 <104035947+EmanuelAM01@users.noreply.github.com>
2022-09-14 12:56:54 -0500

    Delete server.py

--- server.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-#from app.controllers import users_controller, products_controller
-from config import config
-
-config_class = config['development']
-app = create_app(config_class)
-
-if __name__ == "__main__":
-    app.run()
