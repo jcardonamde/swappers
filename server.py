5345555f5e20accf2c722f9ddd420357199bd03c
Angelica Acevedo <angemar2993@hotmail.com>
2022-09-13 22:08:37 -0500

    Se agrega template de ashboard

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from app import create_app
 #from app.controllers import users_controller, products_controller
 from config import config
+from app.controllers import prueba
 
 config_class = config['development']
 app = create_app(config_class)
